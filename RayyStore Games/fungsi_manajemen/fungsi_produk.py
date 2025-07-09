00bc292d03c7ce131f53b0ee7d5536ff7f72a207
raiiiaaaa <aarayyaseva6969@gmail.com>
2025-07-09 17:10:20 +0700

    update fungsi_produk-1
    menambah detail komentar, dll

--- RayyStore Games/fungsi_manajemen/fungsi_produk.py
+++ RayyStore Games/fungsi_manajemen/fungsi_produk.py
@@ -1,26 +1,42 @@
 from fungsi_manajemen.fungsi_util import *
 
+''' MENAMPILKAN SEMUA PRODUK CSV '''
 def tampilkan_produk():
-    produk = load_produk()
+    produk = load_produk() # MENDEFINISI NILAI PRODUK DARI HASIL LOAD FUNCTION
     print("\n========== Daftar Produk ==========")
+    # MENAMPILKAN ISI DATA SECARA PERULANGAN BERUNTUNAN
+
     for p in produk:
         print(f"ID: {p['id']} | Nama: {p['nama']} | Stok: {p['stok']} | Harga: {p['harga']} | Deskripsi: {p['deskripsi']}")
     print('==============================')
 
+''' MENAMBAH PRODUK CSV SESUAI INPUT '''
 def tambah_produk():
-    produk = load_produk()
-    id_produk = input("ID Produk: ")
-    nama = input("Nama Produk: ")
-    stok = input("Stok Awal: ")
-    harga = input("Harga: ")
-    deskripsi = input("Deskripsi: ")
+    while True:
+        produk = load_produk() # MENDEFINISI NILAI PRODUK DARI HASIL LOAD FUNCTION
+
+        # MENGINPUT KATEGORI PRODUK TAMBAHAN
+        id_produk = input("ID Produk: ")
+        nama = input("Nama Produk: ")
+        stok = input("Stok Awal: ")
+        harga = input("Harga: ")
+        deskripsi = input("Deskripsi: ")
+        if id_produk == ' ' or nama == ' ' or stok == ' ' or harga == ' ' or deskripsi == ' ':
+            print('Inputan tidak boleh kosong!')
+        else:
+            break
+
+        # MENAMBAH HASIL INPUTAN KATEGORI 
     produk.append({'id': id_produk, 'nama': nama, 'stok': stok, 'harga': harga, 'deskripsi': deskripsi})
-    simpan_produk(produk)
+    simpan_produk(produk)   # MENYIMPAN HASIL INPUTAN KATEGORI LEWAT FUNCTION
     print("Produk berhasil ditambahkan!")
 
+''' UPDATE PRODUK CSV SESUAI INPUT '''
 def update_produk():
-    produk = load_produk()
+    produk = load_produk() # MENDEFINISI NILAI PRODUK DARI HASIL LOAD FUNCTION
+
     id_cari = input("Masukkan ID Produk: ")
+    
     for p in produk:
         if p['id'] == id_cari:
             print('Masukkan id baru!')
@@ -46,17 +62,7 @@ def update_produk():
             return
     print("Produk tidak ditemukan.")
 
-# def update_harga():
-#     produk = load_produk()
-#     id_cari = input("Masukkan ID Produk: ")
-#     for p in produk:
-#         if p['id'] == id_cari:
-#             p['harga'] = input("Harga baru: ")
-#             simpan_produk(produk)
-#             print("Harga berhasil diperbarui!")
-#             return
-#     print("Produk tidak ditemukan.")
-
+''' MENGHAPUS PRODUK CSV SESUAI INPUT '''
 def hapus_produk():
     produk = load_produk()
     id_hapus = input("ID Produk yang akan dihapus: ")
