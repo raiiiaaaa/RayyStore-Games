553c5818da31ad29d55718705487afe79648087e
raiiiaaaa <aarayyaseva6969@gmail.com>
2025-07-10 21:48:16 +0700

    update la

--- RayyStore Games/laporan.py
+++ RayyStore Games/laporan.py
@@ -3,9 +3,11 @@ from fungsi_manajemen.fungsi_util import load_transaksi
 
 ''' LAPORAN TRANSAKSI '''
 def laporan(tipe):
-    transaksi = load_transaksi()
+    transaksi = load_transaksi() # MENDEFINISI NILAI CSV DARI HASIL LOAD FUNCTION
     print(f"\n========== Laporan {tipe.capitalize()} ========")
-    hari_ini = datetime.datetime.now()
+    hari_ini = datetime.datetime.now() # MENDEFINISI WAKTU HARI INI
+
+    # PERULANGAN BERUNTUN UNTUK PROSES LAPORAN
     for t in transaksi:
         tgl = datetime.datetime.strptime(t['tanggal'], '%Y-%m-%d')
         selisih = (hari_ini - tgl).days
