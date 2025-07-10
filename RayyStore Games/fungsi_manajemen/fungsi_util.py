3051106d49f0c49b9ce484874ebb5889d5d72c45
raiiiaaaa <aarayyaseva6969@gmail.com>
2025-07-10 21:51:40 +0700

    update fungsi_util-2
    tambahan komentar dan function pencarian

--- RayyStore Games/fungsi_manajemen/fungsi_util.py
+++ RayyStore Games/fungsi_manajemen/fungsi_util.py
@@ -5,18 +5,18 @@ transaksi_file = 'transaksi.csv'
 ''' LOAD SYSTEM '''
 def load_produk():
     try:
-        with open(produk_file, mode='r', newline='') as file:   # MEMBUKA FILE PRODUK CSV LALU MEMBERI INISIAL 'file'
-            reader = csv.DictReader(file)   # MENDEFINISI NILAI READER DARI DATA FILE PRODUK CSV
-            return list(reader)     # MENGEMBALIKAN NILAI LIST BERUPA ISIAN DARI FILE PRODUK CSV
-    except FileNotFoundError:   # SYNTAX UNTUK MENGATASI JIKA DATA FILE PRODUK CSV TIDAK DITEMUKAN
+        with open(produk_file, mode='r', newline='') as file:   # MEMBUKA FILE PRODUK.CSV LALU MEMBERI INISIAL 'file'
+            reader = csv.DictReader(file)   # MENDEFINISI NILAI READER DARI DATA FILE PRODUK.CSV
+            return list(reader)     # MENGEMBALIKAN NILAI LIST BERUPA ISIAN DARI FILE PRODUK.CSV
+    except FileNotFoundError:   # SYNTAX UNTUK MENGATASI JIKA DATA DALAM FILE PRODUK.CSV TIDAK DITEMUKAN
         return []   # MENGEMBALIKAN NILAI BERUPA LIST KOSONG
     
 def load_transaksi():
     try:
-        with open(transaksi_file, mode='r', newline='') as file:
-            reader = csv.DictReader(file)
-            return list(reader) # MENGEMBALIKAN NILAI LIST BERUPA ISIAN DARI FILE TRANSAKSI CSV
-    except FileNotFoundError: # SYNTAX UNTUK MENGATASI JIKA DATA DALAM FILE TRANSAKSI CSV TIDAK DITEMUKAN
+        with open(transaksi_file, mode='r', newline='') as file: # MEMBUKA FILE TRANSAKSI.CSV LALU MEMBERI INISIAL 'file'
+            reader = csv.DictReader(file) # MENDEFINISI NILAI READER DARI DATA FILE TRANSAKSI.CSV
+            return list(reader) # MENGEMBALIKAN NILAI LIST BERUPA ISIAN DARI FILE TRANSAKSI.CSV
+    except FileNotFoundError: # SYNTAX UNTUK MENGATASI JIKA DATA DALAM FILE TRANSAKSI.CSV TIDAK DITEMUKAN
         return [] # MENGEMBALIKAN NILAI BERUPA LIST KOSONG
 
 
