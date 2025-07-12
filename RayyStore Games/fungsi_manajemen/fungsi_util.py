f0f28ef933d49281b9f3073ea94b314592a1a1d1
raiiiaaaa <aarayyaseva6969@gmail.com>
2025-07-12 22:52:46 +0700

    update fungsi_util-3
    perubahan kecil komentar dan kategori item

--- RayyStore Games/fungsi_manajemen/fungsi_util.py
+++ RayyStore Games/fungsi_manajemen/fungsi_util.py
@@ -20,22 +20,25 @@ def load_transaksi():
         return [] # MENGEMBALIKAN NILAI BERUPA LIST KOSONG
 
 
+
 ''' SAVE SYSTEM '''
 def simpan_produk(data):
-    with open(produk_file, mode='w', newline='') as file: # MEMBUKA FILE PRODUK CSV LALU MEMBERI INISIAL 'file'
-        fieldnames = ['id', 'nama', 'stok', 'harga', 'deskripsi']   # MENDEFINISI LIST KOSONG SESUAI KATEGORI ISI FILE CSV
-        writer = csv.DictWriter(file, fieldnames=fieldnames)    
-        writer.writeheader()
+    with open(produk_file, mode='w', newline='') as file: # MEMBUKA FILE PRODUK.CSV LALU MEMBERI INISIAL 'file'
+        fieldnames = ['id', 'nama', 'stok', 'harga', 'deskripsi']   # MENDEFINISI KATEGORI YG DITAMBAHKAN PADA FILE CSV
+        writer = csv.DictWriter(file, fieldnames=fieldnames)    # MEMBUAT OBJEK WRITER UNTUK MENULIS PADA FILE PRODUK.CSV
+        writer.writeheader() # MENULIS BARIS PERTAMA HEADER PADA PRODUK.CSV
         
+        # PERULANGAN UNTUK MELAKUKAN ITERASI SETIAP ITEM SEBAGAI BARIS BARU 
         for row in data:
-            writer.writerow(row)
+            writer.writerow(row) # MENULIS TERASI SEBAGAI SATU BARIS DATA
 
 
 def simpan_transaksi(data):
-    with open(transaksi_file, mode='w', newline='') as file: # MEMBUKA FILE PRODUK CSV LALU MEMBERI INISIAL 'file'
-        fieldnames = ['id_transaksi', 'id_produk', 'jumlah', 'total', 'tanggal', 'tipe'] # MENDEFINISI LIST KOSONG SESUAI KATEGORI ISI FILE CSV
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
-        writer.writeheader()
+    with open(transaksi_file, mode='w', newline='') as file: # MEMBUKA FILE TRANSAKSI.CSV LALU MEMBERI INISIAL 'file'
+        fieldnames = ['tanggal','id_transaksi', 'id_produk', 'jumlah', 'game', 'total', 'tipe'] # MENDEFINISI KATEGORI YG DITAMBAHKAN PADA FILE CSV
+        writer = csv.DictWriter(file, fieldnames=fieldnames)    # MEMBUAT OBJEK WRITER UNTUK MENULIS PADA FILE TRANSAKSI.CSV
+        writer.writeheader()    # MENULIS BARIS PERTAMA HEADER PADA PRODUK.CSV
         
+        # PERULANGAN UNTUK MELAKUKAN ITERASI SETIAP ITEM SEBAGAI BARIS BARU 
         for row in data:
-            writer.writerow(row)
+            writer.writerow(row) # MENULIS TERASI SEBAGAI SATU BARIS DATA
